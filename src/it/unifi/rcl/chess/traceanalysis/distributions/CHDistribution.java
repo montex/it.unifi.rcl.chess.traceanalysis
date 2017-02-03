6937173273cd9201cbf84c28ade888e522ff75da
Leonardo Montecchi <lmontecchi@montex.org>
2017-02-03 15:45:21 +0100

    TraceAnalyzer: Added copyright notice

--- src/it/unifi/rcl/chess/traceanalysis/distributions/CHDistribution.java
+++ src/it/unifi/rcl/chess/traceanalysis/distributions/CHDistribution.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2017 Resilient Computing Lab, University of Firenze.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Leonardo Montecchi		lmontecchi@unifi.it
+ ******************************************************************************/
 package it.unifi.rcl.chess.traceanalysis.distributions; 
 
 import it.unifi.rcl.chess.traceanalysis.Trace;
